fb7c19c95b2c7f1612c85f6c33e51afabc054964
Che60 <junyoung833@gmail.com>
2023-12-01 23:12:21 +0900

    중간보고

---
+++ src/main/java/com/example/board/BoardVO.java
@@ -0,0 +1,71 @@
+package com.example.board;
+
+import java.util.Date;
+
+public class BoardVO {
+    private int seq;
+    private String title;
+    private String writer;
+    private String content;
+    private String category;
+    private Date create_date;
+    private int cnt;
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+}
+
